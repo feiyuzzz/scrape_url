fb578440f388cdedb86b3bd22d0eaab12401081a
Eddie <uzhnotbox@outlook.com>
2022-09-04 12:21:07 +0800

    fixed issue

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-use std::fs;
+use std::{fs, io};
 
-fn main() {
-    let url = "http://www.rust-lang.org/";
-    let output = "rust.md";
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut url = String::new();
+    let mut output =  String::new();
+
+    println!("Pls input the url. example: http://www.rust-lang.org/");
+    io::stdin()
+    .read_line(&mut url)?;
+
+    println!("Pls input the output name. example: rust.md");
+    io::stdin()
+    .read_line(&mut output)?;
+
+    let url = url.trim_end();
+    let output = output.trim_end();
 
     println!("Fetching url: {}", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let body = reqwest::blocking::get(url)?.text()?;
 
     println!("Converting html to markdown...");
     let md = html2md::parse_html(&body);
 
-    fs::write(output, md.as_bytes()).unwrap();
+    println!("Writing to file: {}", output);
+
+    fs::write(output, md.as_bytes())?;
     println!("Done!");
+
+    Ok(())
 }
